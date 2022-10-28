956596ccce4190a7e634a40daeb83dcc4c6fdee1
T I M M Y <105170187+Timmydudew@users.noreply.github.com>
2022-10-28 05:07:33 +0100

    Update otp.py

--- otp.py
+++ otp.py
@@ -1,8 +1,42 @@
 import colorama
 import time, sys
+import random
+import webbrowser
 from colorama import Fore, Style
 Green = Fore.GREEN
 Yellow = Fore.YELLOW
 Blue = Fore.BLUE
 
- print(,
+def start()
+   print(f"{Fore.GREEN} Hello dear JT is back")
+   delay_print(f"{Fore.YELLOW}||||||●||||||||||||●||||||")
+   ready = input("are you ready to get otp bypass")
+   if ready==y or ready==yes:
+     vict = input("input victims number")
+   elif vict < 11:
+     print(vict)
+     print("|||loading|||)
+     time.sleep(1)
+     get = input("have you sent the verification code through WhatsApp")
+   if get==yes or get==y
+     print(f"{Fore.GREEN}getting otp verification")
+     rotp = '520568,'235578','596986','428211','153277','247196'
+     choice = random.choice(rotp)
+     print(choice)
+     print("use the code to login")
+     time.sleep(10)
+     print("logged in")
+     time.sleep(2)
+     print("if you have any problems check my github")
+     webbrowser.open("https://github.com/Timmydudew")
+  else:
+     print("There was an error")
+     start()
+
+start()
+     
+     
+
+    
+     
+       
