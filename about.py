854e42de2e5ebf056458d7541ab09d25f86ee78c
T I M M Y <105170187+Timmydudew@users.noreply.github.com>
2022-10-28 16:17:13 +0100

    Create about.py

---
+++ about.py
@@ -0,0 +1,10 @@
+import os
+import webbrowser
+
+def about():
+    print("hey I am JT pls follow my github")
+    print("I have created a lot of repo check my github")
+    webbrowser.open("https://github/Timmydudew")
+
+
+about()
